c18ec848f97c966d620f370073837e82aa288444
AllisRog <58751060+AllisRog@users.noreply.github.com>
2019-12-12 20:32:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ bot.on('message', message=>{
 
     let args = message.content.substring(PREFIX.length).split(" ");
     function assignRole(member2, roleName){
+        member2.addRole("name", "Membre");
         member2.setRoles(["654738611274776576", roleName]);
-        message.channel.sendMessage("I'm here !");
         return member2;
     }
     function level(Membres){

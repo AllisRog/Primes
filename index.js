3dc54d7f48cb383188b231bbe20a87ed099eeb28
AllisRog <58751060+AllisRog@users.noreply.github.com>
2019-12-16 14:02:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -68,26 +68,25 @@ bot.on('message', message=>{
             for (let index = 0; index < Membres.length; index++) {
                 if (args[1] == Membres[index][0]){
                     if (args[2]!=null && parseInt(args[2]) > 0 && Membres[index][2] != message.member.id){
-                        for (let index_2 = 0; index_2 < Membres.length; index_2++) {
-                            if (message.member.id == Membres[index_2][2]){
-                                if (parseInt(args[2])<Membres[index_2][1]){
-                                    Membres[index_2][1] =  Membres[index_2][1] - parseInt(args[2]);
-                                    message.channel.sendMessage("Tu as perdu de l'argent");
-                                    break;
+                                for (let index_2 = 0; index_2 < Membres.length; index_2++) {
+                                    if (message.member.id==Membres[index_2][2]){
+                                        if (parseInt(args[2])<Membres[index_2][1]){
+                                            Membres[index][1] = Membres[index][1] + parseInt(args[2]);
+                                            message.channel.sendMessage("Tu as gagné de l'argent");
+                                            Membres[index_2][1] =  Membres[index_2][1] - parseInt(args[2]);
+                                            message.channel.sendMessage("Tu as perdu de l'argent");
+                                            break;
+                                        }
+                                        else{
+                                            message.channel.sendMessage("Action Impossible.");
+                                            break;
+                                        }
+                                    }
                                 }
+                                }    
                             }  
                         }
-                        Membres[index][1] = Membres[index][1] + parseInt(args[2]);
-                        message.channel.sendMessage("Tu as gagné de l'argent");
-                        break;
-                    }else{
-                        message.channel.sendMessage("Action Impossible.");
-                        break;
-                    }
-                    break;
-                }
-            }
-            break;
+            break;    
         case 'supprime':
             if(!args[1]) return message.reply('Erreur. Définissez le nombre de messages à supprimer (en comptant le votre).')
             if (args[1]<=100){

fd5169d3b6c6bccc46702d9e73abc53c3a91a1e2
AllisRog <58751060+AllisRog@users.noreply.github.com>
2019-12-16 13:45:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,19 +67,15 @@ bot.on('message', message=>{
         case 'transfert':
             for (let index = 0; index < Membres.length; index++) {
                 if (args[1] == Membres[index][0]){
-                    if (args[2]!=null && args[2] <= Membres[index][1] && parseInt(args[2]) > 0 && Membres[index][2] != message.member.id){
+                    if (args[2]!=null && parseInt(args[2]) > 0 && Membres[index][2] != message.member.id){
                         for (let index_2 = 0; index_2 < Membres.length; index_2++) {
-                            if (message.member.id = Membres[index_2][0]){
+                            if (message.member.id = Membres[index_2][2]){
                                 Membres[index_2][1] =  Membres[index_2][1] - parseInt(args[2]);
                             }  
                         }
-                        if (args[1]==Membres[index][0]){
-                            Membres[index][1] = Membres[index][1] + parseInt(args[2]);
-                        }else{
-                            message.channel.sendMessage("Bien tenté. Vous ne pouvez pas vous transférer à vous-même.");
-                        }
+                        Membres[index][1] = Membres[index][1] + parseInt(args[2]);
                     }else{
-                        message.channel.sendMessage("Précisez la valeur à transférer. Celle entrée est incorrecte. (doit être supérieure à zero, et inférieure à vos Berrys)");
+                        message.channel.sendMessage("Action Impossible.");
                         break;
                     }
                     break;

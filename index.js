647c8f3b63f00f6055fbc6ea090f777873cf3855
AllisRog <58751060+AllisRog@users.noreply.github.com>
2020-01-03 21:43:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ bot.on('message', message=>{
             default:
                 valid = false;         
             }
-        
+        console.log(valid);
         return valid;
     }
     // Fonction checkRoles : Permet de changer le rôle d'un utilisateur en fonction de sa prime.
